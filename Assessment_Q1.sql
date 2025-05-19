b463a0f987c2f9995602fffc9ddbb87408a3bc43
Lara-akanni <85748408+Lara-akanni@users.noreply.github.com>
2025-05-19 23:31:01 +0100

    Assessment_Q1.sql
    Added comments.

--- Assessment_Q1.sql
+++ Assessment_Q1.sql
@@ -1,12 +1,18 @@
 SELECT 
 p.owner_id, 
+-- join first name and last name to form name
 CONCAT(u.first_name, ' ', u.last_name) AS name, 
+-- count the number of savings plans where is_regular_savings = 1
 SUM(CASE WHEN p.is_regular_savings = 1 THEN 1 ELSE 0 END) AS savings_count, 
+-- count the number of investments plans where is_a_fund = 1
 SUM(CASE WHEN p.is_a_fund = 1 THEN 1 ELSE 0 END) AS investment_count, 
 SUM(s.confirmed_amount) AS total_deposits
 FROM plans_plan p
+-- join plans table with users table using owner_id and with savings table using plan_id
 INNER JOIN users_customuser u ON p.owner_id = u.id
 INNER JOIN savings_savingsaccount s ON p.id = s.plan_id
 GROUP BY p.owner_id, u.first_name, u.last_name
+-- this will search customers with at least one funded savings plan AND one funded investment plan 
 HAVING savings_count >=1 AND investment_count = 1
+-- order by descending order to show customers with more inflow at the top
 ORDER BY total_deposits DESC;
